ad6353e9ec0627e78d39203641b5b6d10fe3cc22
Iaroslav <yaroslav.da@mail.ru>
2023-12-11 22:39:30 +0300

    init class oparation

--- Models/operation.py
+++ Models/operation.py
@@ -1,2 +1,18 @@
 class Operation:
-    pass
+    def __init__(
+            self,
+            pk: int,
+            date: str,
+            state: str,
+            operation_amount: dict,
+            description: str,
+            from_: str,
+            to: str
+    ):
+        self.pk = pk
+        self.date = date
+        self.state = state
+        self.operation_amount = operation_amount
+        self.description = description
+        self.from_ = from_
+        self.to = to
