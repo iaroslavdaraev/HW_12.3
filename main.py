9fe877ce1925405857dd1d637abc66e7b835a558
Iaroslav <yaroslav.da@mail.ru>
2023-12-11 23:41:08 +0300

    реализовать получение данных

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from config import OPERATIONS_PATH
+from utils import get_operations_instances, get_all_operations
+
+
 def main():
-    pass
+    all_operations = get_all_operations(OPERATIONS_PATH)
+    operations_instances = get_operations_instances(all_operations)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
 
+from Models.operation import Operation
+
 
 def get_all_operations(path):
     """
@@ -9,3 +11,25 @@ def get_all_operations(path):
     """
     with open(path, encoding='utf-8') as file:
         return json.load(file)
+
+
+def get_operations_instances(operations):
+    """
+    Получает экземпляры классы
+    :param operations: pk, date, state, operation_amount, description, from_, to
+    :return: dict operation_instances
+    """
+    operations_instances = []
+    for operation in operations:
+        if operation:
+            operations_instance = Operation(
+                pk=operation['id'],
+                date=operation['date'],
+                state=operation['state'],
+                operation_amount=operation['operationAmount'],
+                description=operation['description'],
+                from_=operation.get('from', ''),
+                to=operation['to'],
+            )
+            operations_instances.append(operations_instance)
+    return operations_instances
